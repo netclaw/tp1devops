5ce064624386f7a4a1051bbb142d19ab15c8c152
netclaw <93434037+netclaw@users.noreply.github.com>
2023-05-06 20:20:09 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        // add modifications
+        // add extra modifications
         System.out.println("Some modifications from Abderafie Chairi");
         
     }
